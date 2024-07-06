9c46bfa257c1ed96d688e91d5f3c470842c920f9
salah mbarki <salahmba004@gmail.com>
2024-07-06 20:33:31 +0100

    tshirt design

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import {proxy} from 'valtio';
+
+const state = proxy({
+intro: true ,
+color: '#EFBD48',
+isLogoTexture:true,
+isFullTexture:false,
+logoDecal:'./threejs.png',
+fullDecal:'./threejs.png',
+
+
+
+
+});
+export default state;
